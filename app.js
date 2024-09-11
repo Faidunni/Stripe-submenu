23b04a436f7fd9bdfcd66286b35738dc01886b04
Faidunni <hazeezfaizat@gmail.com>
2024-09-11 21:56:13 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,8 @@
+import sublinks from "./data.js";
+
+const toggleBtn = document.querySelector(".toggle-btn");
+const closeBtn = document.querySelector(".close-btn");
+const sideWrapper = document.querySelector(".sidebar-wrapper");
+const sidebar = document.querySelector(".sidebar-links");
+
+const linkBtns = [...document.querySelectorAll(".link-btn")];
